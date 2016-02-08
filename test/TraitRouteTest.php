786a2c6e2a137bc347071e897b194668c7983483
Kostyantyn Kedrya <kedryakv@gmail.com>
2016-02-08 19:23:01 +0200

    Add test snipets

---
+++ test/TraitRouteTest.php
@@ -0,0 +1,23 @@
+<?php
+//use SmartRouting\Routing\Helpers\TraitRoute;
+///**
+// * Created by PhpStorm.
+// * User: roach
+// * Date: 2/8/16
+// * Time: 4:47 PM
+// */
+//class TraitRouteTest extends PHPUnit_Framework_TestCase
+//{
+//
+//    public function testConcreteMethod()
+//    {
+//        $mock = $this->getMockForTrait('\SmartRouting\Routing\Helpers\TraitRoute');
+//
+//        $mock->expects($this->any())
+//            ->method('printRoutes')
+//            ->will($this->returnValue(array()));
+//
+//        $this->assertTrue($mock->concreteMethod());
+//    }
+//
+//}
