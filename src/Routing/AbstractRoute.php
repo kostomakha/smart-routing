81834c5c7047d0907071b7477b8b182a49ba1ce0
Kostyantyn Kedrya <kedryakv@gmail.com>
2016-02-19 11:38:49 +0200

    remove unused constructor

--- src/Routing/AbstractRoute.php
+++ src/Routing/AbstractRoute.php
@@ -3,17 +3,8 @@
 namespace SmartRouting\Routing;
 
 
-use SmartRouting\Routes;
-
 abstract class AbstractRoute
 {
-    protected $routesCollection;
-
-    public function __construct()
-    {
-        $this->routesCollection = Routes::getInstance();
-    }
-
     abstract public function findRoute($path, $method);
 
     abstract public function buildRoute($name, array $params = array(), $absolute = false);
